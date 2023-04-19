739c2d0eda708750eced8a125a8b3a7c968f618b
AbdAljawad-Ayah <abdaljawad.ayah94@gmail.com>
2023-04-19 11:10:08 +0300

    fixed some minor designs and exported the users table

--- users.sql
+++ users.sql
@@ -1,10 +1,6 @@
+SET NAMES 'utf8';
 
-                                       
-  Command "db:export" is not defined.  
-                                       
-  Did you mean one of these?           
-      db                               
-      db:seed                          
-      db:wipe                          
-                                       
+INSERT INTO mini_crm.users(id, name, email, email_verified_at, password, two_factor_secret, two_factor_recovery_codes, two_factor_confirmed_at, remember_token, created_at, updated_at, is_admin) VALUES
+(1, 'Admin', 'admin@admin.com', NULL, '$2y$10$lsbKoRQTQ6XfGqCsY4Al/.GfIbQsqJNOYLkfgt9Q42wsdyXRWAKvG', NULL, NULL, NULL, NULL, '2023-04-16 18:31:56', '2023-04-16 18:31:56', 0);
+
 
